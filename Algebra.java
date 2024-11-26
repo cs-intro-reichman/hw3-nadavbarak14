ceeb9f79f0da9767707bf9ee122d036ecb57a744
NADAV\nadav <nadav.barak01@post.runi.ac.il>
2024-11-26 17:23:24 +0200

    Version 1

--- Algebra.java
+++ Algebra.java
@@ -1,67 +1,135 @@
-// Implements algebraic operations and the square root function without using 
-// the Java operations a + b, a - b, a * b, a / b, a % b, and without calling 
-// Math.sqrt. All the functions in this class operate on int values and
-// return int values.
-
 public class Algebra {
-	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
-	}  
+    public static void main(String args[]) {
+        // Tests some of the operations
+        System.out.println(plus(2,3));   // 2 + 3
+        System.out.println(minus(7,2));  // 7 - 2
+           System.out.println(minus(2,7));  // 2 - 7
+         System.out.println(times(3,4));  // 3 * 4
+           System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+           System.out.println(pow(5,3));      // 5^3
+           System.out.println(pow(3,5));      // 3^5
+           System.out.println(div(12,3));   // 12 / 3    
+           System.out.println(div(5,5));    // 5 / 5  
+           System.out.println(div(25,7));   // 25 / 7
+           System.out.println(mod(25,7));   // 25 % 7
+           System.out.println(mod(120,6));  // 120 % 6    
+           System.out.println(sqrt(36));
+        System.out.println(sqrt(263169));
+           System.out.println(sqrt(76123));
+    }  
 
-	// Returns x1 + x2
-	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+    // Returns x1 + x2
+    public static int plus(int a, int b) {
+        int sum = a;
+        if (b > 0) {
+            for (int i = 0; i < b; i++) {
+                sum++;
+            }
+        } else if (b < 0) {
+            for (int i = 0; i < minus(0, b); i++) {
+                sum--;
+            }
+        }
+        return sum;
+    }
 
-	// Returns x1 - x2
-	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+    // Returns x1 - x2
+    public static int minus(int a, int b) {
+        int difference = a;
+        if (b > 0) {
+            for (int i = 0; i < b; i++) {
+                difference--;
+            }
+        } else if (b < 0) {
+            for (int i = b; i < 0; i++) {
+                difference++;
+            }
+        }
+        return difference;
+    }
 
-	// Returns x1 * x2
-	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+    // Returns x1 * x2
+    public static int times(int a, int b) {
+        int product = 0;
+        if (a > 0 && b < 0) {
+            int temp = b;
+            b = a;
+            a = temp;
+        } else if (a < 0 && b < 0) {
+            a = minus(0, a);
+            b = minus(0, b);
+        } else if (a == 0 || b == 0) {
+            return 0;
+        }
 
-	// Returns x^n (for n >= 0)
-	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
-	}
+        for (int i = 0; i < b; i++) {
+            product = plus(product, a);
+        }
+        return product;
+    }
 
-	// Returns the integer part of x1 / x2 
-	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+    // Returns x^n (for n >= 0)
+    public static int pow(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else if (base == 0) {
+            return 0;
+        }
 
-	// Returns x1 % x2
-	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
+        int result = base;
+        for (int i = 0; i < exponent - 1; i++) {
+            result = times(result, base);
+        }
+        return result;
+    }
 
-	// Returns the integer part of sqrt(x) 
-	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
-	}	  	  
+    // Returns the integer part of x1 / x2 
+    public static int div(int dividend, int divisor) {
+        if (dividend == 0) {
+            return 0;
+        }
+
+        boolean isNegative = false;
+        if (dividend < 0 && divisor < 0) {
+            dividend = minus(0, dividend);
+            divisor = minus(0, divisor);
+        } else if (dividend < 0 && divisor > 0) {
+            dividend = minus(0, dividend);
+            isNegative = true;
+        } else if (divisor < 0 && dividend > 0) {
+            divisor = minus(0, divisor);
+            isNegative = true;
+        }
+
+        int quotient = 0;
+        while (dividend >= divisor) {
+            dividend = minus(dividend, divisor);
+            quotient++;
+        }
+        while (dividend < 0) {
+            dividend = plus(dividend, divisor);
+            quotient--;
+        }
+
+        return isNegative ? minus(0, quotient) : quotient;
+    }
+
+    // Returns x1 % x2
+    public static int mod(int dividend, int divisor) {
+        boolean isNegative = dividend < 0;
+        dividend = isNegative ? minus(0, dividend) : dividend;
+        while (dividend > 0 && (dividend > divisor || dividend == divisor)) {
+            dividend = minus(dividend, divisor);
+        }
+        return isNegative ? minus(0, dividend) : dividend;
+    }	
+
+    // Returns the integer part of sqrt(x) 
+    public static int sqrt(int number) {
+        int guess = 1;
+        while (times(guess, guess) < number) {
+            guess++;
+        }
+        return times(guess, guess) == number ? guess : guess - 1;
+    }	  	  
 }
--- Anagram.java
+++ Anagram.java
@@ -1,49 +1,105 @@
 /** Functions for checking if a given string is an anagram. */
 public class Anagram {
-	public static void main(String args[]) {
-		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+    public static void main(String args[]) {
+        // Tests the isAnagram function.
+        System.out.println(isAnagram("silent", "listen")); // true
+        System.out.println(isAnagram("William Shakespeare", "I am a weakish speller")); // true
+        System.out.println(isAnagram("Madam Curie", "Radium came")); // true
+        System.out.println(isAnagram("Tom Marvolo Riddle", "I am Lord Voldemort")); // true
 
-		// Tests the preProcess function.
-		System.out.println(preProcess("What? No way!!!"));
-		
-		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-		
-		// Performs a stress test of randomAnagram 
-		String str = "1234567";
-		Boolean pass = true;
-		//// 10 can be changed to much larger values, like 1000
-		for (int i = 0; i < 10; i++) {
-			String randomAnagram = randomAnagram(str);
-			System.out.println(randomAnagram);
-			pass = pass && isAnagram(str, randomAnagram);
-			if (!pass) break;
-		}
-		System.out.println(pass ? "test passed" : "test Failed");
-	}  
+        // Tests the preProcess function.
+        System.out.println(preProcess("What? No way!!!"));
 
-	// Returns true if the two given strings are anagrams, false otherwise.
-	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
-	}
-	   
-	// Returns a preprocessed version of the given string: all the letter characters are converted
-	// to lower-case, and all the other characters are deleted, except for spaces, which are left
-	// as is. For example, the string "What? No way!" becomes "whatnoway"
-	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
-	} 
-	   
-	// Returns a random anagram of the given string. The random anagram consists of the same
-	// characters as the given string, re-arranged in a random order. 
-	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
-	}
+        // Tests the randomAnagram function.
+        System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
+
+        // Performs a stress test of randomAnagram 
+        String testString = "1234567";
+        Boolean pass = true;
+        //// 10 can be changed to much larger values, like 1000
+        for (int i = 0; i < 10; i++) {
+            String randomAnagram = randomAnagram(testString);
+            System.out.println(randomAnagram);
+            pass = pass && isAnagram(testString, randomAnagram);
+            if (!pass) break;
+        }
+        System.out.println(pass ? "test passed" : "test Failed");
+    }
+
+    public static boolean isPunctuation(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        String punctuation = " .,:;!?\"'()-{}[]<>";
+        char character = str.charAt(0);
+        return punctuation.indexOf(character) != -1;
+    }
+
+    public static boolean isSame(String first, String second) {
+        return first.equals(second.toLowerCase()) || first.equals(second.toUpperCase());
+    }
+
+    // Returns true if the two given strings are anagrams, false otherwise.
+    public static boolean isAnagram(String first, String second) {
+        if (first == null || second == null) {
+            return false;
+        }
+        if (first.isEmpty() && second.isEmpty()) {
+            return true;
+        }
+        String[] firstElements = first.split("");
+        int firstLength = first.length();
+        int secondLength = second.length();
+        String[] secondElements = second.split("");
+        boolean found;
+        for (int i = 0; i < firstLength; i++) {
+            if (!isPunctuation(firstElements[i])) {
+                found = false;
+                for (int j = 0; j < secondLength && !found; j++) {
+                    if (!isPunctuation(secondElements[j]) && isSame(firstElements[i], secondElements[j])) {
+                        found = true;
+                        secondElements[j] = "";
+                    }
+                }
+                if (!found) {
+                    return false;
+                }
+            }
+        }
+        for (int j = 0; j < secondLength; j++) {
+            if (!secondElements[j].equals("") && !isPunctuation(secondElements[j])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Returns a preprocessed version of the given string: all the letter characters are converted
+    // to lower-case, and all the other characters are deleted, except for spaces, which are left
+    // as is. For example, the string "What? No way!" becomes "whatnoway"
+    public static String preProcess(String str) {
+        if (str == null || str.isEmpty()) return str;
+        StringBuilder processedString = new StringBuilder();
+        for (char character : str.toCharArray()) {
+            if (!isPunctuation(String.valueOf(character))) {
+                processedString.append(Character.toLowerCase(character));
+            } else if (character == ' ') {
+                processedString.append(character);
+            }
+        }
+        return processedString.toString();
+    }
+
+    // Returns a random anagram of the given string. The random anagram consists of the same
+    // characters as the given string, re-arranged in a random order.
+    public static String randomAnagram(String str) {
+        String[] characters = str.split("");
+        for (int i = characters.length - 1; i > 0; i--) {
+            int randomIndex = (int) (Math.random() * (i + 1));
+            String temp = characters[i];
+            characters[i] = characters[randomIndex];
+            characters[randomIndex] = temp;
+        }
+        return String.join("", characters);
+    }
 }
